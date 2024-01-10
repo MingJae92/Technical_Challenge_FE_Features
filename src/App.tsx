cd454d9a8e87d40480264114ba4aff4cb0da3c04
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-01-10 23:56:34 +0000

    Type script added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface AppProps {
+  // Define your prop types here
+}
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
+
+export default App;
